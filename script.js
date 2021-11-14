9483d170655cda161262593f41cd194548d2ca97
MTheuws <89788123+MTheuws@users.noreply.github.com>
2021-11-14 18:05:40 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+const pizzaMaken = function () {
+    console.log('maak het deeg voor de bodem');
+    console.log('neem een bolletje deen en rol dit uit tot een dunne bodem');
+    console.log('bestrijk de hele bodem met een laagje tomatensaus');
+    console.log('strooi ingredienten naar keuze uit over de met tomatensaus bedekte bodem');
+    console.log('strooi hier kaas overheen');
+    console.log('bak de pizza in enkele minuten gaar in de oven');
+};
+
+const sushiMaken = function () {
+    console.log('kook de rijst');
+    console.log('neem een sushivel');
+    console.log('bestrijk 3/4 met een laagje rijst');
+    console.log('leg in het midden over de hele lengte de vulling zoals zalm, avocado of bijvoorbeeld kip');
+    console.log('rol het sushivel met rijst en vulling op');
+    console.log('snij de rol in stukjes');
+};
+
+sushiMaken();
+sushiMaken();
+sushiMaken();
+pizzaMaken();
+pizzaMaken();
